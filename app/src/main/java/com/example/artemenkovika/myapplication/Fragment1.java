bb4591819176409c231be346c64d493feb25eb6c
Artemenko Vika <vikaiisus2012@yandex.ua>
2014-10-27 21:38:56 +0200

    Navigation Drawer

--- app/src/main/java/com/example/artemenkovika/myapplication/Fragment1.java
+++ app/src/main/java/com/example/artemenkovika/myapplication/Fragment1.java
@@ -4,42 +4,62 @@ import android.os.Bundle;
 import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.*;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.app.ActionBar;
 
-public class Fragment1 extends Fragment {
-    TextView txt;
+public class Fragment1 extends Fragment implements OnClickListener {
+    public TextView txt;
+    private Button btn1;
+    private Button btn2;
+    private Button btn3;
+    private Button btn4;
+    private Button btn5;
+    private ActionBar actionBar;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.fragment_fragment1, null);
-        txt = (TextView) v.findViewById(R.id.textView);
-        Button btn1 = (Button) v.findViewById(R.id.button1);
-        Button btn2 = (Button) v.findViewById(R.id.button2);
-        Button btn3 = (Button) v.findViewById(R.id.button3);
-        btn1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                txt.setText("Нажата кнопка \"Кнопка 1\"");
-            }
-        });
-        btn2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                txt.setText("Нажата кнопка \"Кнопка 2\"");
-            }
-        });
-        btn3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                txt.setText("Нажата кнопка \"Кнопка 3\"");
-            }
-        });
-
-        return v;
+        return inflater.inflate(R.layout.fragment_fragment1, container, false);
     }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+        actionBar = getActivity().getActionBar();
+        txt = (TextView) getActivity().findViewById(R.id.textView);
+        btn1 = (Button) getActivity().findViewById(R.id.button1);
+        btn2 = (Button) getActivity().findViewById(R.id.button2);
+        btn3 = (Button) getActivity().findViewById(R.id.button3);
+        btn4 = (Button) getActivity().findViewById(R.id.bHide);
+        btn5 = (Button) getActivity().findViewById(R.id.bShow);
+        btn1.setOnClickListener(this);
+        btn2.setOnClickListener(this);
+        btn3.setOnClickListener(this);
+        btn4.setOnClickListener(this);
+        btn5.setOnClickListener(this);
+    }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button1:
+                txt.setText("Нажата кнопка \"Кнопка 1\"");
+                break;
+            case R.id.button2:
+                txt.setText("Нажата кнопка \"Кнопка 2\"");
+                break;
+            case R.id.button3:
+                txt.setText("Нажата кнопка \"Кнопка 3\"");
+                break;
+            case R.id.bHide:
+               actionBar.hide();
+                break;
+            case R.id.bShow:
+                actionBar.show();
+                break;
+        }
+    }
 }
--- app/src/main/java/com/example/artemenkovika/myapplication/Fragment2.java
+++ app/src/main/java/com/example/artemenkovika/myapplication/Fragment2.java
@@ -1,15 +1,43 @@
 package com.example.artemenkovika.myapplication;
 
 import android.os.Bundle;
-import android.app.Fragment;
+import android.app.ListFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.ListAdapter;
+import android.widget.ArrayAdapter;
 
-public class Fragment2 extends Fragment {
+public class Fragment2 extends ListFragment {
+    private static final String data[] = new String[]{"First Screen", "Second Screen", "Change The Picture"};
+    private TextView text;
+    public static ListAdapter adapter;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        adapter = new ArrayAdapter<String>(getActivity(),
+                android.R.layout.simple_list_item_single_choice, data);
+        setListAdapter(adapter);
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_fragment2, container, false);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        text = (TextView) getActivity().findViewById(R.id.textView1);
+    }
+
+    @Override
+    public void onListItemClick(ListView l, View v, int position, long id) {
+        String selectedItem = getListView().getItemAtPosition(position).toString();
+        text.setText(selectedItem);
     }
 }
